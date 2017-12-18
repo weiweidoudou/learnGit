cc6e25313bc9d9c1da91643e1b2926e9cd1feffc
zhangwei <1432664223@qq.com>
2017-12-18 17:27:18 +0800

    4

--- js/index.js
+++ js/index.js
@@ -49,17 +49,17 @@
 //     $("html").height("590px");
 //   }
 // })
-function action() {
-  var mySwiper = new Swiper('.swiper-container', {
-    direction: 'vertical',
-    onSlideChangeEnd: function (swiper) {
-      swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
-    },
-    onInit: function (swiper) { //Swiper2.x的初始化是onFirstInit
-      swiperAnimateCache(swiper); //隐藏动画元素 
-      setTimeout(function () {
-        swiperAnimate(swiper); //初始化完成开始动画
-      }, 200);
-    }
-  });
-}
+// function action() {
+//   var mySwiper = new Swiper('.swiper-container', {
+//     direction: 'vertical',
+//     onSlideChangeEnd: function (swiper) {
+//       swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+//     },
+//     onInit: function (swiper) { //Swiper2.x的初始化是onFirstInit
+//       swiperAnimateCache(swiper); //隐藏动画元素 
+//       setTimeout(function () {
+//         swiperAnimate(swiper); //初始化完成开始动画
+//       }, 200);
+//     }
+//   });
+// }
